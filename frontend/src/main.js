aac120f77daf929423342efc012431aadaf4b584
Everson Clei <everson.clei@gmail.com>
2021-06-07 18:42:01 -0300

    Commit Home

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -12,9 +12,6 @@ import router from './config/router'
 
 Vue.config.productionTip = false
 
-// TEMPORARIO!
-//require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkV2ZXJzb24gQ2xlaSIsImVtYWlsIjoiZXZlcnNvbi5jbGVpQGdtYWlsLmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE2MjE3MTU4MzIsImV4cCI6MTYyMTk3NTAzMn0.cS4Zk17T7sCuUKh2uA0CNQAE3k1qquEUTlnZ9v09bpY'
-
 new Vue({
   store,
   router,
